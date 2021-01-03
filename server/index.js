1f8ce9836916b239fce9b324eadc7b3273f23dfb
Andrew Sanchez <a.vincent.sanchez@gmail.com>
2021-01-03 00:24:28 +0000

    fixed review path

--- server/index.js
+++ server/index.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const routes = [
+  "about",
+  "bookreadings",
+  "blog",
+  "grown-up-resources",
+  "for-authors",
+  "contact",
+];
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.sendFile(path.join(__dirname + "/wpPages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/about.html"));
+});
+
+app.get("/bookreadings", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/bookreadings.html"));
+});
+
+app.get("/reviews", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/reviews.html"));
+});
+
+app.get("/grown-up-resources", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/grown-up-resources.html"));
+});
+
+app.get("/for-authors", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/for-authors.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname + "/wpPages/contact.html"));
 });
 
 const PORT = process.env.PORT || 8000;
