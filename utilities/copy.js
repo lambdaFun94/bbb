a759067b1d83593a455f7f22ddd42eb214d0ce61
Andrew Sanchez <a.vincent.sanchez@gmail.com>
2021-01-03 00:09:30 +0000

    first commit

---
+++ utilities/copy.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const baseURL = "http://www.barbsbookbuddies.com/home";
+const wpSite = axios.create({
+  baseURL,
+});
+
+const routes = [
+  "about",
+  "bookreadings",
+  "blog",
+  "grown-up-resources",
+  "for-authors",
+  "contact",
+];
+
+const getPage = async (route) => {
+  const response = await wpSite.get(`/${route}`);
+  fs.writeFileSync(`./wpPages/${route}.html`, response.data);
+};
+
+// routes.forEach(getPage);
+getPage("");
